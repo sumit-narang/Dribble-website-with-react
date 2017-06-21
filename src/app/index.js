08562ad00fcd9ce11446a10b54729762e1bc92a5
Sumit Narang <sumitnarang.web@gmail.com>
2017-06-21 18:47:04 +0530

    added 1st component

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,22 @@
-console.log("hello from sumit narang");
+import React from "react";
+import ReactDOM from "react-dom";
+
+//Everything is a component in react unlike element in html
+//Layout Component
+class Layout extends React.Component{
+	render(){
+		//JSX part wrapped in paranthese will convert html to js created element
+		//var div1 = document.createElement('div');
+		//div1.innerHTML = "Hello";
+		//so jsx will transpile to above
+		return(
+			<h1>How is it going</h1>
+		);
+	}
+}
+
+const app = document.getElementById('app');
+//get the app element from DOM
+//get the layout component
+//render layout component into the app
+ReactDOM.render(<Layout/>,app);
